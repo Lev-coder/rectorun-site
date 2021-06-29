12ddd826f0e932ad5de0c807968faf37758dc01d
Lev-coder <54110434+Lev-coder@users.noreply.github.com>
2021-06-29 15:07:56 +0300

    make architecture

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+
 import './App.css';
 
-class App extends React.Component
+import Head from './sections/head/Head';
+import Menu from './sections/menu/Menu';
+import Comments from './sections/comments/Comments';
+import Ordering from './sections/ordering/Ordering';
+import Footer form './sections/footer/Foote';
+
+export default class App extends React.Component
 {
   render()
   {
     return(
       <div>
-        start
+        <Head />
       </div>
     )
   }
 }
-
-
-export default App;
