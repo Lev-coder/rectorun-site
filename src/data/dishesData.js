f75eed2c57096bdf3c4ec67f80cfabd6e2d73108
Lev-coder <54110434+Lev-coder@users.noreply.github.com>
2021-06-30 16:50:17 +0300

    add cards and slider

---
+++ src/data/dishesData.js
@@ -0,0 +1,45 @@
+
+let dishesDaya = {
+
+    defaultData:
+    {
+        name: "",
+        price: "",
+        imgSrc: ""
+    },
+
+    dishes: [
+        {
+            name: "Мафия метровая",
+            price: "349 грн",
+            imgSrc: ""
+        },
+        {
+            name: "Мафия",
+            price: "189 грн",
+            imgSrc: ""
+        },
+        {
+            name: "Поло метровая",
+            price: "299 грн",
+            imgSrc: ""
+        },
+        {
+            name: "Пицца Охотничья XXL",
+            price: "299 грн",
+            imgSrc: ""
+        },
+        {
+            name: "Пицца Охотничья",
+            price: "139 грн",
+            imgSrc: ""
+        },
+        {
+            name: "Мясная гриль метровая",
+            price: "299 грн",
+            imgSrc: ""
+        },
+    ]
+}
+
+export default dishesDaya;
--- src/sections/menu/Menu.js
+++ src/sections/menu/Menu.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import dishesData from '../../data/dishesData';
+
+import BaseSlider from '../../elements/sliders/base_slider/BaseSlider'
+import BaseCard from '../../elements/cards_dish/base_card/BaseCard';
+
 export default class Menu extends React.Component {
     render() {
         return (
-            <div>
-                Menu
+            <div className="menu">
+                <div className="content">
+                    <div className="dishes">
+
+                        {
+                            dishesData.dishes.map(dish => {
+                                console.log(dish)
+                                return < BaseCard
+                                    name={dish.name}
+                                    price={dish.price}
+                                    imgSrc={dish.imgSrc}
+                                />;
+                            })
+                        }
+
+                    </div>
+                    < BaseSlider />
+                </div>
             </div >
         )
     }
+
+
 }
