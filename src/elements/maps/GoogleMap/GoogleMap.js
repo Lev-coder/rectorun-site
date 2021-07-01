c536141301d5bb6b738520ced095c50aafe183e2
Lev-coder <54110434+Lev-coder@users.noreply.github.com>
2021-07-01 16:05:05 +0300

    make tamplate for map

---
+++ src/elements/maps/GoogleMap/GoogleMap.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styleForGoogleMap from './GoogleMap.module.css';
+
+export default class GoogleMap extends React.Component
+{
+    render()
+    {
+        return(
+            <div className={styleForGoogleMap.map}>
+
+            </div>
+        )
+    }
+}
--- src/sections/ordering/Ordering.js
+++ src/sections/ordering/Ordering.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import BaseForm from '../../elements/forms/base_form/BaseForm';
+import GoogleMap from '../../elements/maps/GoogleMap/GoogleMap';
 
 export default class Ordering extends React.Component {
     render() {
         return (
             <div>
                 < BaseForm />
+                < GoogleMap />
             </div >
         )
     }
