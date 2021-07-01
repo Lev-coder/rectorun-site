2651ce318385df6729d9ea4c3e1a120e0b0f4b17
Lev-coder <54110434+Lev-coder@users.noreply.github.com>
2021-07-01 14:54:42 +0300

    make comments templete

--- src/sections/comments/Comments.js
+++ src/sections/comments/Comments.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
+import styleForComments from './Commnents.module.css'
+
 export default class Comments extends React.Component {
     render() {
+
+        console.log(`->>>${styleForComments}`);
+
         return (
-            <div>
-                Comments
+            <div className={styleForComments.comments}>
+                <div className={styleForComments.content}>
+                    <div className={styleForComments.commentBody}>
+
+                        <div className={styleForComments.leftStep}>
+
+                        </div>
+
+                        <div className={styleForComments.review}>
+                            <div className={styleForComments.avatar}>
+
+                            </div>
+                            <div className={styleForComments.comment}>
+
+                            </div>
+                        </div>
+
+                        <div className={styleForComments.rightStep}>
+
+                        </div>
+
+                    </div>
+                </div>
             </div >
         )
     }
--- src/sections/head/Head.js
+++ src/sections/head/Head.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import styleForHeader from './Head.module.css';
 
 
